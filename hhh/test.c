2cbf03a1f9f66a64939d391015de813e37ecbefa
walter <wwybiji@163.com>
2019-07-31 10:46:14 +0800

    sub_branch commit again

--- hhh/test.c
+++ hhh/test.c
@@ -24,10 +24,10 @@ int main(int argc, char *argv[])
 {
   int i =0;
 
-  printf("\n hhhhhhhhhhhhhhhh\n");
+  
   printf("this is the sub_branch code \n");
  
-  printf(" sub_branch add new");
+  
  
   return 1;
 

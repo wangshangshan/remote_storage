617738e32694945336e3aaf05dfb2fa5ee89286f
walter <wwybiji@163.com>
2019-07-31 10:38:00 +0800

    master commit

---
+++ hhh/test.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019年07月31日 10时35分23秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main(int argc, char *argv[])
+{
+  int i =0;
+
+  printf("\n hhhhhhhhhhhhhhhh\n");
+  printf("this is the master code \n");
+
+ 
+  return 1;
+
+
+
+}
+
+
